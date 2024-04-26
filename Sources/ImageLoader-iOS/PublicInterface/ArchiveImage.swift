f38150e75935c8fbb110a1052f00c0bf44978b86
LEE HANWE <mobiledev@kakao.com>
2024-04-26 14:11:04 +0900

    feat: resizable, renderingMode 메소드 추가

--- Sources/ImageLoader-iOS/PublicInterface/ArchiveImage.swift
+++ Sources/ImageLoader-iOS/PublicInterface/ArchiveImage.swift
@@ -13,15 +13,36 @@ import Kingfisher
 public struct ArchiveImage: View {
   
   public let imageUrl: URL?
+  private var capInsets: EdgeInsets = EdgeInsets()
+  private var resizingMode: Image.ResizingMode = .stretch
+  private var renderingMode: Image.TemplateRenderingMode?
   
   public var body: some View {
-    ZStack {
-      KFImage(self.imageUrl)
-    }
+    KFImage(self.imageUrl)
+      .resizable(capInsets: capInsets, resizingMode: resizingMode)
+      .renderingMode(renderingMode)
   }
   
   public init(_ url: URL?) {
     self.imageUrl = url
   }
   
+}
+
+extension ArchiveImage {
+  public func resizable(
+    capInsets: EdgeInsets = EdgeInsets(),
+    resizingMode: Image.ResizingMode = .stretch
+  ) -> ArchiveImage {
+    var newImage = self
+    newImage.capInsets = capInsets
+    newImage.resizingMode = resizingMode
+    return newImage
+  }
+  
+  public func renderingMode(_ renderingMode: Image.TemplateRenderingMode?) -> ArchiveImage {
+    var newImage = self
+    newImage.renderingMode = renderingMode
+    return newImage
+  }
 }
